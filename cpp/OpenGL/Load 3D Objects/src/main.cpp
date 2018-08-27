e79eff2c84ca0d1027523ab32e8caaa328e72157
wesleymirada <wymirandac@gmail.com>
2018-08-27 00:00:00 -0400

    Apresentando os objetos 3D em uma janela OpenGL

--- cpp/OpenGL/Load 3D Objects/src/main.cpp
+++ cpp/OpenGL/Load 3D Objects/src/main.cpp
@@ -30,7 +30,6 @@ struct Coord3d {
 	}
 };
 
-
 struct Face {
 	int facenum;
 	bool quad; // topologia eh Quad ou Triângulo?
@@ -113,6 +112,110 @@ Face getFace(string s, bool isQuad)
 
 
 
+int carregaObjeto(const char* nomeArq)
+{
+	vector<string> linhas;//linhas do arquivo
+	vector<Coord3d> vertice;
+	vector<Coord3d> normal;
+	vector<Face> faces;
+
+	ifstream arq;
+
+	arq.open(nomeArq);
+	if (!arq.is_open())
+		cout << "Erro ao abrir o arquivo!" << endl;
+
+
+	while (arq.good())
+	{
+		string linha;
+		getline(arq, linha);
+		linhas.push_back(linha);
+	}
+
+	for (int i = 0; i < linhas.size(); i++)
+	{
+		if (linhas[i][0] == '#')
+			continue;
+		else if (linhas[i][0] == 'v' && (linhas[i])[1] == ' ')
+		{
+			vertice.push_back(getVertex(linhas[i]));
+		}
+		else if ((linhas[i][0] == 'v') && (linhas[i][1] == 'n'))
+		{
+			normal.push_back(getNormal(linhas[i]));
+		}
+		else if (linhas[i][0] == 'f')
+		{
+			int a, b, c, d, e;
+			if (count(linhas[i].begin(), linhas[i].end(), ' ') == 4)
+			{
+				faces.push_back(getFace(linhas[i], true));
+			}
+			else
+			{
+				faces.push_back(getFace(linhas[i], false));
+			}
+		}
+	}
+
+	//Desenha
+	int num;
+	num = glGenLists(1);
+	glNewList(num, GL_COMPILE);
+	for (int i = 0; i < faces.size(); i++)
+	{
+		if (faces[i].quad)
+		{
+			glBegin(GL_QUADS);
+			glNormal3f(normal[faces[i].facenum - 1].x, normal[faces[i].facenum - 1].y, normal[faces[i].facenum - 1].z);
+			glVertex3f(vertice[faces[i].vertices[0] -1].x, vertice[faces[i].vertices[0] - 1].y, vertice[faces[i].vertices[0] - 1].z);
+			glVertex3f(vertice[faces[i].vertices[1] -1].x, vertice[faces[i].vertices[1] - 1].y, vertice[faces[i].vertices[1] - 1].z);
+			glVertex3f(vertice[faces[i].vertices[2] -1].x, vertice[faces[i].vertices[2] - 1].y, vertice[faces[i].vertices[2] - 1].z);
+			glVertex3f(vertice[faces[i].vertices[3] -1].x, vertice[faces[i].vertices[3] - 1].y, vertice[faces[i].vertices[3] - 1].z);
+			glEnd();
+		}
+		else {
+			glBegin(GL_TRIANGLES);
+			glNormal3f(normal[faces[i].facenum - 1].x, normal[faces[i].facenum - 1].y, normal[faces[i].facenum - 1].z);
+			glVertex3f(vertice[faces[i].vertices[0] - 1].x, vertice[faces[i].vertices[0] - 1].y, vertice[faces[i].vertices[0] - 1].z);
+			glVertex3f(vertice[faces[i].vertices[1] - 1].x, vertice[faces[i].vertices[1] - 1].y, vertice[faces[i].vertices[1] - 1].z);
+			glVertex3f(vertice[faces[i].vertices[2] - 1].x, vertice[faces[i].vertices[2] - 1].y, vertice[faces[i].vertices[2] - 1].z);
+
+			glEnd();
+		}
+	}
+	glEndList();
+
+	// Apagar o vetor
+	vertice.clear();
+	normal.clear();
+	faces.clear();
+
+	//
+	cout << num;
+	return num;
+}
+
+
+void desenha()
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	float pos[] = { 2,2,5,1 };
+	glLightfv(GL_LIGHT0, GL_POSITION, pos);
+
+	glTranslatef(0, 0, -5);
+	glRotatef(ang, 1, 1, 1);
+
+	glCallList(objeto);
+
+	glutSwapBuffers();
+
+}
+
 void inicia3D()
 {
 
@@ -134,7 +237,7 @@ void inicia3D()
 	//glFogf(GL_FOG_END,5.0);
 	//float colorFog[]={0.6,0.6,0.6,1.0};
 	//glFogfv(GL_FOG_COLOR,colorFog);
-	objeto = carregaObjeto("monkey.obj");
+	objeto = carregaObjeto("3D Objects/monkey.obj");
 }
 
 void teclado(unsigned char key, int x, int y)
