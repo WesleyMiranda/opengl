def961a7871a8cbfcbf6d630279d9e544e73d440
wesleymirada <wymirandac@gmail.com>
2018-10-03 00:00:00 -0400

    Criando uma classe que trabalhe com matrizes

--- cpp/Calculo Numerico/Matrizes/matriz.cpp
+++ cpp/Calculo Numerico/Matrizes/matriz.cpp
@@ -105,3 +105,62 @@ void divideMatriz(int l, double num, Matriz& M)
 	for (int j = 0; j < M.coluna; j++)
 		M.valor[l][j] /= num;
 }
+
+
+Matriz rref(Matriz M)
+{
+	int lead = 0;
+	int rowCount = M.linha;
+	int columnCount = M.coluna;
+
+	for (int r = 0; r < rowCount; r++)
+	{
+		if (columnCount <= lead)
+			break;
+		int i = r;
+		while (saoIguais(M.valor[i][lead], 0.0))
+		{
+			i++;
+			if (i == rowCount)
+			{
+				i = r;
+				lead++;
+				if (lead == columnCount)
+					return M;
+			}
+		}//while
+
+		trocarLinhas(i, r, &M);
+		double pivo = M.valor[r][lead];
+		divideMatriz(r, pivo, M);
+		for (int k = 0; k < M.linha; k++)
+		{
+			if (k != r)
+			{
+				pivo = -M.valor[k][lead];
+				for (int j = 0; j < M.coluna; j++)
+					M.valor[k][j] += pivo * M.valor[r][j];
+			}
+		}
+		lead++;
+	}//prox r
+
+	return M;
+}
+
+Matriz criarIdentidade(int x) {
+
+	Matriz Temp;
+	Temp.linha = Temp.coluna = x;
+
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < x; j++) {
+			if (i == j)
+				Temp.valor[i][j] = 1.0;
+			else
+				Temp.valor[i][j] = 0.0;
+		}
+	}
+
+	return Temp;
+}
