6be7849e945a80274cbcc43da9b73473e51f5b5c
wesleymirada <wymirandac@gmail.com>
2018-11-23 00:00:00 -0400

    Recriando o algoritmo de deteccao de contorno com o gradiente em X e Y

--- cpp/OpenCV/Recriando algoritmo de Deteccao de Contorno/main.cpp
+++ cpp/OpenCV/Recriando algoritmo de Deteccao de Contorno/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int Masc(uchar V[2]);
 uchar Grad(uchar X[2], uchar Y[2]);
+Mat Blur(Mat M);
+Mat Threshold(Mat M);
 const uchar Tresh = 8;
 
 int main()
@@ -35,6 +37,35 @@ int main()
 
 }
 
+Mat Blur(Mat M)
+{
+	uchar X[2], Y[2];
+	Mat Aux(M.rows, M.cols, CV_8U);
+
+	for (int i = 0; i < M.rows; i++)
+	{
+		for (int j = 0; j < M.cols; j++)
+		{
+			if (i == 0 || i == M.rows - 1|| j == 0 || j == M.cols - 1)
+			{
+				Aux.at<uchar>(i, j) = 0;
+			}
+			else
+			{
+				X[0] = M.at<uchar>(i, j - 1);
+				X[1] = M.at<uchar>(i, j + 1);
+				Y[0] = M.at<uchar>(i - 1, j);
+				Y[1] = M.at<uchar>(i + 1, j);
+
+				Aux.at<uchar>(i, j) = Grad(X, Y);
+			}
+		}
+	}
+
+	Aux = Threshold(Aux);
+
+	return Aux;
+}
 
 uchar Grad(uchar X[2], uchar Y[2])
 {
@@ -52,4 +83,22 @@ int Masc(uchar V[2])
 	M[1] = -1;
 
 	return  abs(V[0] * M[0] + V[1] * M[1]);
+}
+
+Mat Threshold(Mat M)
+{
+	uchar aux;
+
+	for (int i = 0; i < M.rows; i++)
+	{
+		for (int j = 0; j < M.cols; j++)
+		{
+			aux = M.at<uchar>(i, j);
+			if (aux >= Tresh)
+				M.at<uchar>(i, j) = 0;
+			else
+				M.at<uchar>(i, j) = 255;
+		}
+	}
+	return M;
 }
