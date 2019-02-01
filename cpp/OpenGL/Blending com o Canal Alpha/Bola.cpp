d75b0297a7e0eac381315623f1e3f26751e83b8c
wesleymirada <wymirandac@gmail.com>
2019-02-01 00:00:00 -0400

    Criando blend com OpenGL

--- cpp/OpenGL/Blending com o Canal Alpha/Bola.cpp
+++ cpp/OpenGL/Blending com o Canal Alpha/Bola.cpp
@@ -19,4 +19,29 @@ Vetor::Vetor(float x_, float y_)
     y=y_;
 }
 
+Bola::Bola()
+{
+    set_x(LARGURA/2);
+    set_y(ALTURA/2);
+    set_raio(8);
+
+	m_cor[0]=1;
+	m_cor[1]=0;
+	m_cor[2]=0;
+	m_cor[3]=0.3;
+}
+
+Bola::Bola(float x, float y)
+{
+	destroi=false;
+	set_x(x);
+	set_y(y);
+	set_raio(5);
+	alpha=1;
+	m_cor[0]=1;
+	m_cor[1]=0;
+	m_cor[2]=0;
+	m_cor[3]=alpha;
+	veloc=Vetor(aleatorio(-0.5,0.5),aleatorio(2,2.5));
+}
 
