f59ae31ebb9fef8f8edc45df111f0d5cac529b73
wesleymirada <wymirandac@gmail.com>
2019-04-09 00:00:00 -0400

    Interpolacao com perceptron

---
+++ cpp/Neural Network/Perceptron Interpolation/src/Perceptron.cpp
@@ -0,0 +1,113 @@
+#include "Perceptron.h"
+#include<cstdlib>
+#include<GL/freeglut.h>
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+
+using std::cout;
+using std::endl;
+
+Perceptron::Perceptron()
+{
+    //weights.resize(3);// reserva 3
+    weightsNum=6;
+    for(unsigned i=0;i<weightsNum;i++)//wo,w1,w2,w3,w4 e w5
+    {
+        double w=random(-1,1);
+        weights.push_back(w);
+    }
+    drawCurve=0;//default
+
+}
+
+Perceptron::~Perceptron()
+{
+    //dtor
+
+}
+
+void Perceptron::print()
+{
+    using std::setprecision;
+    cout<< '\r';
+    for(unsigned i=0;i<weights.size();i++)
+        cout<<"w["<<i<<"]="<<setprecision(3)<< std::fixed<< weights[i] << " ";
+    cout<<std::flush;
+}
+
+double Perceptron::random(double a, double b)
+{
+    double random = ((double) rand()) / (double) RAND_MAX;
+    double diff = b - a;
+    double r = random * diff;
+    return a + r;
+}
+
+ double Perceptron::dotProduct(std::vector<double> u, std::vector<double> v)
+{
+    double sum=0;
+    for(unsigned i=0;i<v.size();i++)
+        sum+=u[i]*v[i];
+
+    return sum;
+}
+
+double Perceptron::predict(std::vector<double> inputs)
+{
+    double output=dotProduct(inputs,weights);
+
+    return output;
+}
+
+double Perceptron::activation(double x) // Activation Function
+{
+
+
+    switch(drawCurve)
+    {
+    case 1:
+        return 1/(1+exp(-x)); // sigmoid(x)
+    case 2:
+        return (exp(x)-exp(-x))/(exp(x)+exp(-x));//tanh(x)
+    default:
+            return x; // linear
+    }
+}
+
+void Perceptron::training(Knots data)
+{
+    std::vector<double> inputs;
+    int m=data.size();
+
+    for(unsigned j=0;j<weights.size();j++)
+    {
+        double dW=0;
+        for(unsigned i=0;i<data.size();i++)
+        {
+
+            for(unsigned k=0;k<weightsNum;k++)
+                inputs.push_back(pow(data[i].x,k));// enter:1,x,x^2,..,x^n
+
+
+
+            double finded=activation(predict(inputs));
+            double goal=activation(data[i].y);
+           // float actived=1.0/(1.0+exp(-finded));
+            //float goal =1.0/(1.0+exp(-data[i].y));
+            double error=finded-goal;
+            //float error=actived-goal;
+
+            dW+=error*pow(data[i].x,j);
+            //somaError+=error*inputs[j];
+
+
+            inputs.clear();
+        }
+        weights[j]-=learningCoef*1/m*dW;
+    }
+
+}
+
+
