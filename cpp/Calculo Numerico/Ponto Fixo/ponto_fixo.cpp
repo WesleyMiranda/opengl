f0398a765752e274fa5108fd28d105522ea11ee8
wesleymirada <wymirandac@gmail.com>
2019-06-23 00:00:00 -0400

    Classe para uso de ponto fixo

--- cpp/Calculo Numerico/Ponto Fixo/ponto_fixo.cpp
+++ cpp/Calculo Numerico/Ponto Fixo/ponto_fixo.cpp
@@ -21,3 +21,26 @@ void ponto_fixo::cvtFloat_Fixo()
    // return f*pow(2,escala);
 }
 
+float ponto_fixo::cvtFixo_Float(int i)
+{
+    //return i/pow(2,escala);
+    return (float)i/(1<<mescala);
+}
+
+void ponto_fixo::calcula()
+{
+    cvtFloat_Fixo();
+    mresult=cvtFixo_Float(mfixo);
+}
+
+float ponto_fixo::show()
+{
+    calcula();
+    return mresult;
+}
+
+void ponto_fixo::mprint()
+{
+    calcula();
+    std::cout<< mresult<<std::endl;
+}
